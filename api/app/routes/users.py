fcf1f124fe2816e01ae4361588a74cc5e71efd83
cowanweks <cowannwekesa@gmial.com>
2024-07-18 20:35:44 +0300

    Updated API

--- api/app/routes/users.py
+++ api/app/routes/users.py
@@ -1,6 +1,9 @@
 from flask import Blueprint, request, jsonify
-from app.controllers.user import new_user, get_users, delete_user, update_user
-
+from uuid import uuid4
+from app.models import db, User
+from app.forms.user import UserRegistrationForm, UserUpdateForm, UserLoginForm, UserUpdatePasswordForm
+from app.utils.login_utils import hash_password, verify_password
+from sqlalchemy.exc import IntegrityError, SQLAlchemyError
 
 # User blueprint
 user_route = Blueprint("user_route", __name__, url_prefix="/api/v1/users")
@@ -10,50 +13,101 @@ user_route = Blueprint("user_route", __name__, url_prefix="/api/v1/users")
 def new_user_route():
     """New User"""
 
-    valid, msg = new_user(request.form)
+    try:
+        new_user_form = UserRegistrationForm(request.form)
 
-    if valid:
-        return jsonify(msg=msg), 201
+        if new_user_form.validate():
+            db.session.execute(
+                db.insert(User).values(
+                    user_id=str(uuid4()),
+                    staff_no=new_user_form.staff_no.data,
+                    username=new_user_form.username.data,
+                    roles=new_user_form.roles.data,
+                    password=hash_password(new_user_form.password.data)
+                )
+            )
+            db.session.commit()
+            return True, "Successfully Created new User!"
 
-    return jsonify(msg=msg), 500
+        else:
+            print(new_user_form.errors)
+            return False, new_user_form.errors
+
+    except IntegrityError as ex:
+        print(ex)
+        return False, "User already exists!"
+
+
+@user_route.route("/<string:user_id>", methods=["GET"])
+def get_user_route(user_id: str):
+    """Get the users"""
+
+    try:
+        users = (
+            db.session.query(User).order_by(User.user_id).all()
+        )
+
+        serialized_users = [user.serialize() for user in users]
+        return jsonify(serialized_users), 200
+
+    except Exception as ex:
+        print(ex)
+        return jsonify("Database error occurred!"), 500
 
 
 @user_route.route("/", methods=["GET"])
 def get_users_route():
     """Get the users"""
 
-    valid, response = get_users(request.args.get("user_id"))
+    try:
+        users = db.session.query(User).order_by(User.user_id).all()
 
-    if valid:
-        return jsonify(data=response), 200
+        serialized_users = [user.serialize() for user in users]
+        return jsonify(serialized_users), 200
 
-    return jsonify(msg=response), 500
+    except Exception as ex:
+        print(ex)
+        return jsonify("Database error occurred!"), 500
 
 
-@user_route.route("/", methods=["PUT", "PATCH"])
-def update_user_route():
+@user_route.route("/<string:user_id>", methods=["PUT", "PATCH"])
+def update_user_route(user_id: str):
     """Update User"""
 
-    valid, response = update_user(request.args.get("user_id"), request.form)
+    try:
+        updated_user_form = UserUpdateForm(request.form)
 
-    if valid:
-        return jsonify(msg="Successfully Updated User!"), 201
+        if updated_user_form.validate():
+            db.session.execute(
+                db.update(User)
+                .where(User.user_id == user_id)
+                .values(roles=updated_user_form.roles.data)
+            )
+            db.session.commit()
+            return True, "Successfully Updated User!"
 
-    else:
-        return jsonify(msg="Database error occurred!"), 500
+        else:
+            return False, updated_user_form.errors
+
+    except SQLAlchemyError as ex:
+        print(ex)
+        return False, "Database error occurred!"
 
 
-@user_route.route("/", methods=["DELETE"])
-def delete_user_route():
+@user_route.route("/<string:user_id>", methods=["DELETE"])
+def delete_user_route(user_id: str):
     """Delete User"""
 
-    valid, response = delete_user(request.args.get("user_id"))
+    try:
+        db.session.execute(db.delete(User).where(User.user_id == user_id))
+        db.session.commit()
+        db.session.close()
+        return True, "Successfully Deleted User!"
 
-    if valid:
-        return jsonify(msg="Successfully Deleted User!"), 200
-
-    else:
-        return jsonify(msg="Database error occurred!"), 500
+    except SQLAlchemyError as ex:
+        print(ex)
+        db.session.close()
+        return False, "Database error occurred!"
 
 
 def signin_user_route():
