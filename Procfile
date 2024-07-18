6f49cbc02be9438234cd4c5d68f6ced95c8ec823
cowanweks <cowannwekesa@gmial.com>
2024-07-18 19:39:41 +0300

    Updated API

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn --workers=1 --chdir api run:flask_app
--- api/run.py
+++ api/run.py
@@ -1,17 +1,24 @@
+import os.path
+from flask import send_from_directory
 from app import create_app
-from waitress import serve
+
+
+flask_app = create_app()
+flask_app.static_folder = "static"
+
+
+@flask_app.route("/")
+def index_page():
+    print("Hello")
+    return send_from_directory('static', 'index.html')
+
+
+@flask_app.route("/<path:path>")
+def static_file(path):
+    return flask_app.send_static_file(path)
 
 
 def main():
-    """Create the flask application"""
-    flask_app = create_app()
-
-    if flask_app.config.get("ENV") == "production":
-        try:
-            serve(app=flask_app, host="0.0.0.0", port=5000)
-        except Exception as ex:
-            print(str(ex))
-
     flask_app.run(host=flask_app.config.get("HOST"), port=flask_app.config.get("PORT"))
 
 
