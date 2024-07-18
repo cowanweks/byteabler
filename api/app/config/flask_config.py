5e97c2b8713b33eddd9550e7956d98b50c2c4e41
cowanweks <cowannwekesa@gmial.com>
2024-07-18 20:42:34 +0300

    Updated API

--- api/app/config/flask_config.py
+++ api/app/config/flask_config.py
@@ -39,5 +39,5 @@ class ProductionConfig(Config):
     DEBUG = False
     HOST = "0.0.0.0"
     TESTING = True
-    SQLALCHEMY_DATABASE_URI = "postgres://wqrzpmlf:COpWVXHpfaB-c-udEI7lcWxD1tcp8Ho8@ruby.db.elephantsql.com/wqrzpmlf"
+    SQLALCHEMY_DATABASE_URI = "postgresql://wqrzpmlf:COpWVXHpfaB-c-udEI7lcWxD1tcp8Ho8@ruby.db.elephantsql.com/wqrzpmlf"
 
