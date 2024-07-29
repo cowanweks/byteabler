97ba85f143cba81c62150e749180e5abde57f855
cowanweks <cowannwekesa@gmail.com>
2024-07-29 15:49:27 +0300

    Updated desktop and mobile

--- desktop/ui/src/types.ts
+++ desktop/ui/src/types.ts
@@ -1,5 +1,5 @@
-export const API_URL = "http://127.0.0.1:3000/api";
-// export const API_URL = "https://byteabler-8c063875d0ca.herokuapp.com/api";
+// export const API_URL = "http://127.0.0.1:3000/api";
+export const API_URL = "https://byteabler-8c063875d0ca.herokuapp.com/api";
 
 
 export type Response = {
